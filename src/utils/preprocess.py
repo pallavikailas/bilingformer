c0e16e786abe77b3ff3449048452b9d270db1907
Pallavi Kailas <119298061+pallavikailas@users.noreply.github.com>
2024-01-31 18:40:25 +0530

    Update preprocess.py

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -1,26 +1,27 @@
 import json
 import pandas as pd
-from sklearn.utils.class_weight import compute_class_weight
 
 def load_data(file_path):
+    # Load data from a JSON file
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
 def prepare_data(data, emotion_to_label, trigger_to_label):
-    utterances = []
-    emotions = []
-    triggers = []
+    # Prepare data by extracting utterances, emotions, and triggers
+    # and converting them into a format suitable for training
+    utterances, emotions, triggers = [], [], []
     for item in data:
         utterances.extend(item['utterances'])
         emotions.extend(item['emotions'])
         triggers.extend(item['triggers'])
     triggers = [convert_to_int_or_nan(t) for t in triggers]
-    labels = [emotion_to_label[emotion] for emotion in emotions]
+    labels = [emotion_to_label[e] for e in emotions]
     trigger_labels = [trigger_to_label.get(t, t) for t in triggers]
     return pd.DataFrame({'utterances': utterances, 'labels': labels, 'triggers': trigger_labels})
 
 def convert_to_int_or_nan(t):
+    # Convert trigger values to integers or NaN
     if not pd.isna(t):
         try:
             return int(t)
